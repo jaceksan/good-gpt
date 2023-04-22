a59061ba6cc32a9cf4f8637c67ff03d5c5578444
Mara3l <Mara3l@proton.me>
2023-04-22 09:50:36 +0200

    git V1.0

--- git_poc.py
+++ git_poc.py
@@ -1,13 +1,22 @@
 from github_client import *
 
 if __name__ == "__main__":
+
+    branch_name="test-branch5"
+    commit_message="Test commit V2.1"
+    update_commit_message="Test commit V2.2"
+    file_name="new_awesome_file.txt"
+    file_content="Some random sample content\nThis is a second line\nThis is a third line"
+    updated_file_content=file_content+"\nThis is a fourth line"
     # Create a new branch
-    new_branch = create_branch('test-branch')
-    print(new_branch)
+    new_branch = create_branch(branch_name)
+    #print(new_branch)
 
     # Commit to the new branch
-    commit_to_branch('test-branch', 'README.md', 'Commit to test branch')
+    create_file_in_branch(file_name=file_name,file_content=file_content,commit_message=update_commit_message, branch_name=branch_name)
+
+    update_file_in_branch(branch_name=branch_name, file_path=file_name, updated_content=updated_file_content, commit_message=commit_message)
 
     # Create a pull request
-    pull_request = create_pull_request('Test Pull Request', 'This is a test pull request', 'test-branch', 'main')
+    pull_request = create_pull_request('Test Pull Request V1.2', 'This is a test pull request V1.2', branch_name, 'main')
     print(pull_request)
--- github_client.py
+++ github_client.py
@@ -44,7 +44,7 @@ def create_pull_request(title, body, head_branch, base_branch):
 
     return pull_request
 
-def commit_to_branch(branch_name, file_path, commit_message):
+def update_file_in_branch(branch_name, file_path, updated_content ,commit_message):
     # Authentication
     g = Github(os.environ['GITHUB_PERSONAL_ACCESS_TOKEN'])
 
@@ -59,12 +59,29 @@ def commit_to_branch(branch_name, file_path, commit_message):
     # Get file contents and update it
     file = repo.get_contents(file_path, ref=branch_name)
     file_content = file.decoded_content.decode('utf-8')
-    updated_content = file_content + '\nSome changes.'
+    # updated_content = file_content + '\nSome changes.'
 
-    # Create a new commit
+    # Create a new commit with updated file
+
     repo.update_file(file_path, commit_message, updated_content, file.sha, branch=branch_name)
 
     return "Changes committed to branch: " + branch_name
+
+def create_file_in_branch(file_name:str, file_content:str, commit_message:str, branch_name:str):
+    # create a Github object using the provided access token
+ # Authentication
+    g = Github(os.environ['GITHUB_PERSONAL_ACCESS_TOKEN'])
+
+    # Set user and repository variables
+    user = os.environ['GITHUB_REPO_OWNER']
+    repo_name = os.environ['GITHUB_REPO_NAME']
+
+    # Get repository and branch
+    repo = g.get_user(user).get_repo(repo_name)
+
+    # create a new file
+    repo.create_file(file_name, commit_message, file_content, branch=branch_name)
+
 
 
 def read_pull_request_comments(pr_number):
