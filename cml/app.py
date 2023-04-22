1d2604087d2c6442a5eba08c6275c063fea4b84d
Jacek <jacek@gooddata.com>
2023-04-22 16:39:14 +0200

    FIX: EXAMPLES_DIR

--- cml/app.py
+++ cml/app.py
@@ -10,7 +10,7 @@ import pathlib
 
 # define password
 PASSWORD = os.environ.get("PASSWORD")
-EXAMPLES_DIR = pathlib.Path(__name__).parent.absolute() / "examples"
+EXAMPLES_DIR = pathlib.Path("../examples")
 
 
 # create function to check if password is correct
