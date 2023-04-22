be3fc1534d48def2040cc11cf9b1e5b2716bdc05
GoodGPT <131490648+GoodGPT@users.noreply.github.com>
2023-04-22 17:04:24 +0200

    ssglinechart-kickoff

---
+++ apps/ssglinechart/app.py
@@ -0,0 +1,18 @@
+# Download CSV file
+url = 'https://raw.githubusercontent.com/epogrebnyak/ssg-dataset/main/data/ssg.csv'
+df = pd.read_csv(url)
+
+# Calculate sum of stars by created (year)
+df['created'] = pd.to_datetime(df['created'])
+df['year'] = df['created'].dt.year
+df = df.groupby('year').agg({'stars': 'sum'}).reset_index()
+
+# Display line chart
+chart = alt.Chart(df).mark_line().encode(
+    x='year',
+    y='stars'
+).properties(
+    width=800,
+    height=400
+)
+st.altair_chart(chart)
