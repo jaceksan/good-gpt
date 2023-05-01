d90d6899d73a5b82ccd01cfe230d858917fcadd5
GoodGPT <131490648+GoodGPT@users.noreply.github.com>
2023-05-01 21:19:44 +0200

    Resolving PR comments

--- apps/Some_analytics/app.py
+++ apps/Some_analytics/app.py
@@ -1,15 +1,20 @@
-# Download CSV file
+# Download CSV file from the URL
+# The CSV file contains data about static site generators (SSGs) with columns:
+# 'name', 'url', 'created', 'stars', 'forks', 'issues', 'license', 'language', 'description'
 url = "https://raw.githubusercontent.com/epogrebnyak/ssg-dataset/main/data/ssg.csv"
 df = pd.read_csv(url)
 
-# Read file to data frame
+# Read file to data frame and convert 'created' column to datetime format, keeping only the year
 df['created'] = pd.to_datetime(df['created']).dt.year
+
+# Group the data frame by 'created' (year) and calculate the sum of 'stars' for each year
 df_grouped = df.groupby('created')['stars'].sum().reset_index()
 
-# Calculate sum of stars by created (year) and display it as line chart
+# Create a line chart to display the sum of stars by created (year)
 chart = alt.Chart(df_grouped).mark_line().encode(
     x='created',
     y='stars'
 )
 
+# Display the line chart in the Streamlit app
 st.altair_chart(chart)
